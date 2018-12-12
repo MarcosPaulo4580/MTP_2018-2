6b0f07df5279c0f5dc37268d1c6f049951048df2
MarcosPaulo4580 <42421038+MarcosPaulo4580@users.noreply.github.com>
2018-12-12 19:30:56 -0200

    Update P3.c

--- P3.c
+++ P3.c
@@ -1,18 +1,16 @@
-/*P3.c/
-/Marcos Paulo Arruda Freitas/
-/11811EEL011*/
+/*P3.c*/
+/*Marcos Paulo Arruda Freitas*/
+/*11811EEL011*/
 #include <stdio.h>
 int main ()
 {
 	char bits[256];
-	int i, cum = 0;
-	fflush(stdin);
-	gets(bits);
+	int i = 0, cum = 0;
+	scanf("%s", bits);
 	for(i = 0; bits[i] != '\0'; i++)
 	{
-		if(bits[i] > 47 && bits[i] < 58)
-			cum = cum*10 + (bits[i] - 48);
+		if(bits[i] >= '0' && bits[i] <= '9')
+			cum = cum*10 + (bits[i] - '0');
 	}
 	printf("\n%d", cum);
-	return 0;
 }
